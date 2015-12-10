bf5fab340a4f07fd9f8ae766c390b7677c8cebb3
uzura <wakabayashita@firstserver.co.jp>
2015-12-11 02:52:23 +0900

    マイグレーションファイルを修正
    謎のテーブルが作られてしまっていたため修正

---
+++ db/migrate/20151210105510_create_posts.rb
@@ -0,0 +1,24 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table "posts", force: :cascade do |t|
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.text     "text"
+      t.integer  "user_id"
+    end
+
+    add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+    create_table "users", force: :cascade do |t|
+      t.string   "name"
+      t.string   "color",           default: "red"
+      t.string   "password"
+      t.string   "password_digest"
+      t.integer  "post_id"
+      t.datetime "created_at",                      null: false
+      t.datetime "updated_at",                      null: false
+    end
+
+    add_index "users", ["post_id"], name: "index_users_on_post_id"
+  end
+end
